9e8c6865f03ca7847a1b63bfe88c7aff49ba32ab
Richard <richlyon@mac.com>
2021-11-02 12:05:10 +0000

    Code formatting

--- include/kwHeltecWifikit32.h
+++ include/kwHeltecWifikit32.h
@@ -1,109 +1,91 @@
 #ifndef H_KWHELTECWIFIKIT32
 #define H_KWHELTECWIFIKIT32
 
-#include <Arduino.h>
-#include <Wire.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include <RTClib.h>
 #include "SSD1306Ascii.h"
 #include "SSD1306AsciiWire.h"
+#include <Arduino.h>
+#include <PubSubClient.h>
+#include <RTClib.h>
+#include <WiFi.h>
+#include <Wire.h>
 
 // OLED pins
 #define I2C_ADDRESS 0x3C
-#define PIN_RST 16
-#define PIN_SDA 4
-#define PIN_SCL 15
+#define PIN_RST     16
+#define PIN_SDA     4
+#define PIN_SCL     15
 
 // CHECK THESE !!!     <-----------------------------
-#define MQTT_SOCKET_TIMEOUT 1
+#define MQTT_SOCKET_TIMEOUT         1
 #define MQTT_RECONNECT_TIME_SECONDS 1
 
-struct HeltecConfig
-{
-  const char *ssid;
-  const char *pwd;
-  IPAddress mqtt_host;
-  bool rotateDisplay;
-  const char *firmwareVersion;
+struct HeltecConfig {
+  const char* ssid;
+  const char* pwd;
+  IPAddress   mqtt_host;
+  bool        rotateDisplay;
+  const char* firmwareVersion;
   std::string topicRoot;
 };
 
-struct dataField
-{
+struct dataField {
   std::string topicString;
   std::string fieldName;
   std::string units;
 };
 
-struct Command
-{
-  std::string commandString;
-  std::string payload;
-  bool didReceive;
-};
-
-
-class kwHeltecWifikit32 
-{
-public:
-
+class kwHeltecWifikit32 {
+ public:
   kwHeltecWifikit32( HeltecConfig config );
-  
+
   // Data field methods
-  uint8_t registerField(std::string fieldName, std::string units, std::string topicName, std::string sensorName);
-  uint8_t registerMetaTopic(std::string topicName);
-  uint8_t registerCommandTopic(std::string commandName);
+  uint8_t registerField( std::string fieldName, std::string units,
+                         std::string topicName, std::string sensorName );
+  uint8_t registerMetaTopic( std::string topicName );
 
-  // Command topics
-  bool didReceiveCommand(uint8_t commandID);
-
-  
   // Initialise
   void init();
-  
+
   // WiFi / MQTT methods
-  void publish(uint8_t fieldID, uint16_t data);
-  void publish(uint8_t fieldID, float data);
-  
+  void publish( uint8_t fieldID, uint16_t data );
+  void publish( uint8_t fieldID, float data );
+
   // Real Time Clock methods
   bool isMidnight();
-  
+
   // Display methods
   void update( uint8_t fieldID, uint16_t data );
   void update( uint8_t fieldID, float data );
   void update( uint8_t fieldID, const char* message );
 
   void run();
-  
-  char deviceID[16] = {0};
 
-  std::vector<dataField> dataTopics;
+  char                     deviceID[16] = { 0 };
+  std::vector<dataField>   dataTopics;
   std::vector<std::string> metaTopics;
-  std::vector<Command> commandTopics;
+  std::vector<std::string> commands;
+  uint8_t                  statusTopicID;
 
-  uint8_t statusTopicID;
-  
-  bool hasRTC = false;       // true if an RTC module is present
-  bool rtcWasAdjusted = false; // true if the RTC has been initialised with a time
+  bool hasRTC = false;  // true if an RTC module is present
+  bool rtcWasAdjusted =
+      false;  // true if the RTC has been initialised with a time
 
-private:
-
+ private:
   HeltecConfig config;
-  void getMacAddress();
-  bool initWiFi(const char* SSID, const char* PWD);
-  void initMTTQ(IPAddress mqtt_host);
-  void updateSystemStatus(std::string statusMessage);
-  static void mqttCallback(char* topic, byte* payload, unsigned int length);
-  boolean mqttReconnect();
-  void setUpForm();
+  void         getMacAddress();
+  bool         initWiFi( const char* SSID, const char* PWD );
+  void         initMTTQ( IPAddress mqtt_host );
+  void         updateSystemStatus( std::string statusMessage );
+  static void  mqttCallback( char* topic, byte* payload, unsigned int length );
+  boolean      mqttReconnect();
+  void         setUpForm();
 
-  bool didInitialiseMTTQ = false;
-  long lastReconnectAttempt = 0;
+  bool    didInitialiseMTTQ = false;
+  long    lastReconnectAttempt = 0;
   uint8_t maxRows;
   uint8_t maxCols;
 };
 
-void clearValue(uint8_t row);
+void clearValue( uint8_t row );
 
 #endif
--- src/kwHeltecWifikit32.cpp
+++ src/kwHeltecWifikit32.cpp
@@ -1,291 +1,259 @@
 #include <kwHeltecWifikit32.h>
 #include <kwNeoTimer.h>
 
-char buf[10] = {0};
-char timeString[8] = {0};
+char buf[10] = { 0 };
+char timeString[8] = { 0 };
 
 uint8_t col0 = 0;  // First value column
 uint8_t col1 = 0;  // Last value column.
 
 SSD1306AsciiWire oled;
-WiFiClient wifiClient;
-PubSubClient mqttClient(wifiClient);
-RTC_DS1307 ds1307;
+WiFiClient       wifiClient;
+PubSubClient     mqttClient( wifiClient );
+RTC_DS1307       ds1307;
 
 kwNeoTimer updateTimeTimer = kwNeoTimer();
 
-// PUBLIC ///////////////////////////////////////////////////////////////////////
+// PUBLIC
+// ///////////////////////////////////////////////////////////////////////
 
 // kwHeltecWifikit32 constructor
 kwHeltecWifikit32::kwHeltecWifikit32( HeltecConfig config ) : config{ config }
 {
-    getMacAddress();
-    statusTopicID = registerMetaTopic("status");
-    pinMode(LED, OUTPUT);
+  getMacAddress();
+  statusTopicID = registerMetaTopic( "status" );
+  pinMode( LED, OUTPUT );
 }
 
-// Initalise the display, WiFi, and MQTT 
+// Initalise the display, WiFi, and MQTT
 void kwHeltecWifikit32::init()
 {
-    Wire.begin(PIN_SDA, PIN_SCL);
-    Wire.setClock(400000L);
+  Wire.begin( PIN_SDA, PIN_SCL );
+  Wire.setClock( 400000L );
 
-    hasRTC = ds1307.begin(&Wire);
-    if (hasRTC && ! ds1307.isrunning())
-    {
-        ds1307.adjust(DateTime(F(__DATE__), F(__TIME__)));
-        rtcWasAdjusted = true;
-    } 
+  hasRTC = ds1307.begin( &Wire );
+  if ( hasRTC && !ds1307.isrunning() )
+  {
+    ds1307.adjust( DateTime( F( __DATE__ ), F( __TIME__ ) ) );
+    rtcWasAdjusted = true;
+  }
 
-    oled.begin(&Adafruit128x64, I2C_ADDRESS, PIN_RST);
-    oled.setFont(Callibri15);
-    oled.setLetterSpacing(2);
-    maxRows = oled.displayHeight() / (oled.fontRows() * 8); // 4
-    maxCols = oled.displayWidth() / oled.fontWidth();       // 13
-    oled.displayRemap(config.rotateDisplay);
+  oled.begin( &Adafruit128x64, I2C_ADDRESS, PIN_RST );
+  oled.setFont( Callibri15 );
+  oled.setLetterSpacing( 2 );
+  maxRows = oled.displayHeight() / ( oled.fontRows() * 8 );  // 4
+  maxCols = oled.displayWidth() / oled.fontWidth();          // 13
+  oled.displayRemap( config.rotateDisplay );
 
-    // topicRoot = config.topicRoot;
+  // topicRoot = config.topicRoot;
 
-    setUpForm();
+  setUpForm();
 
-    updateSystemStatus( deviceID );
-    delay( 5000 );
+  updateSystemStatus( deviceID );
+  delay( 5000 );
 
-    initWiFi( config.ssid, config.pwd );
-    initMTTQ( config.mqtt_host );
+  initWiFi( config.ssid, config.pwd );
+  initMTTQ( config.mqtt_host );
 
-    updateTimeTimer.set(1000);
+  updateTimeTimer.set( 1000 );
 }
-
 
 // Display the labeled data at the specified row
 void kwHeltecWifikit32::setUpForm()
 {
-    oled.clear();
-    // Setup form and find longest labels
-    for (uint8_t i = 0; i < dataTopics.size(); ++i)
-    {
-        const char *fieldName = dataTopics[i].fieldName.c_str();
-        oled.println(fieldName);
-        uint8_t w = oled.strWidth(fieldName);
-        col0 = col0 < w ? w : col0; 
-    }
+  oled.clear();
+  // Setup form and find longest labels
+  for ( uint8_t i = 0; i < dataTopics.size(); ++i )
+  {
+    const char* fieldName = dataTopics[i].fieldName.c_str();
+    oled.println( fieldName );
+    uint8_t w = oled.strWidth( fieldName );
+    col0 = col0 < w ? w : col0;
+  }
 
-    col0 += 3;
-    col1 = col0 + oled.strWidth("4000") + 2;
+  col0 += 3;
+  col1 = col0 + oled.strWidth( "4000" ) + 2;
 
-    // Print units
-    for (uint8_t i = 0; i < dataTopics.size(); ++i)
-    {
-        oled.setCursor(col1 + 1, i * oled.fontRows());
-        oled.print(dataTopics[i].units.c_str());
-    }
+  // Print units
+  for ( uint8_t i = 0; i < dataTopics.size(); ++i )
+  {
+    oled.setCursor( col1 + 1, i * oled.fontRows() );
+    oled.print( dataTopics[i].units.c_str() );
+  }
 }
 
-
 // Register data topic
-uint8_t kwHeltecWifikit32::registerField(std::string fieldName, std::string units, std::string topicName, std::string sensorName)
+uint8_t kwHeltecWifikit32::registerField( std::string fieldName,
+                                          std::string units,
+                                          std::string topicName,
+                                          std::string sensorName )
 {
-    std::string topicString = config.topicRoot + "/" + "data" + "/" + topicName + "/" + sensorName + "/" + deviceID;
-    dataTopics.push_back( dataField{ topicString, fieldName, units } );
-    
-    return dataTopics.size() - 1;
+  std::string topicString = config.topicRoot + "/" + "data" + "/" + topicName +
+                            "/" + sensorName + "/" + deviceID;
+  dataTopics.push_back( dataField{ topicString, fieldName, units } );
+
+  return dataTopics.size() - 1;
 }
 
 // register meta topic
-uint8_t kwHeltecWifikit32::registerMetaTopic(std::string topicName)
+uint8_t kwHeltecWifikit32::registerMetaTopic( std::string topicName )
 {
-    std::string topicString = config.topicRoot + "/" + "meta" + "/" + topicName + "/" + deviceID;
-    metaTopics.push_back(topicString);
+  std::string topicString =
+      config.topicRoot + "/" + "meta" + "/" + topicName + "/" + deviceID;
+  metaTopics.push_back( topicString );
 
-    return metaTopics.size() - 1;
-}
-
-uint8_t kwHeltecWifikit32::registerCommandTopic(std::string commandName)
-{
-    std::string commandString = config.topicRoot + "/" + "command" + "/" + commandName + "/" + deviceID;
-    commandTopics.push_back( Command{ commandString, "", false } );
-
-    return commandTopics.size() - 1;
+  return metaTopics.size() - 1;
 }
 
 // Initialise the Wifi - return true if successful
-bool kwHeltecWifikit32::initWiFi(const char* wifi_ssid, const char* wifi_pwd)
+bool kwHeltecWifikit32::initWiFi( const char* wifi_ssid, const char* wifi_pwd )
 {
-    updateSystemStatus("[->] WiFi");
+  updateSystemStatus( "[->] WiFi" );
 
-    WiFi.disconnect();
-    WiFi.mode(WIFI_MODE_STA);
-    WiFi.begin(wifi_ssid);
-    
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-        delay(500);
-    };
+  WiFi.disconnect();
+  WiFi.mode( WIFI_MODE_STA );
+  WiFi.begin( wifi_ssid );
 
-    updateSystemStatus("WiFi OK");
-    delay(500);
+  while ( WiFi.status() != WL_CONNECTED ) { delay( 500 ); };
 
-    return WiFi.status() == WL_CONNECTED;
+  updateSystemStatus( "WiFi OK" );
+  delay( 500 );
+
+  return WiFi.status() == WL_CONNECTED;
 }
 
 // Initialise MTTQ
-void kwHeltecWifikit32::initMTTQ(IPAddress mqtt_host)
+void kwHeltecWifikit32::initMTTQ( IPAddress mqtt_host )
 {
-    mqttClient.setServer(mqtt_host, 1883);
-    mqttClient.setCallback(mqttCallback);
-    didInitialiseMTTQ = true;
+  mqttClient.setServer( mqtt_host, 1883 );
+  mqttClient.setCallback( mqttCallback );
+  didInitialiseMTTQ = true;
 }
 
 // Return true if it is midnight
 bool kwHeltecWifikit32::isMidnight()
 {
-    if (hasRTC)
-    {
-        DateTime now = ds1307.now();
-        return now.hour() == 0 && now.minute() == 0 && now.second() == 0;
-    }
-    else
-    {
-        return false;
-    }
-} 
-
-// Publish 
-
-void kwHeltecWifikit32::publish(uint8_t fieldID, uint16_t data)
-{
-    sprintf(buf, "%d", data);
-    const char* topic = dataTopics[fieldID].topicString.c_str();
-    mqttClient.publish(topic, buf);
+  if ( hasRTC )
+  {
+    DateTime now = ds1307.now();
+    return now.hour() == 0 && now.minute() == 0 && now.second() == 0;
+  } else
+  {
+    return false;
+  }
 }
 
-void kwHeltecWifikit32::publish(uint8_t fieldID, float data)
+// Publish
+
+void kwHeltecWifikit32::publish( uint8_t fieldID, uint16_t data )
 {
-    sprintf(buf, "%.1f", data);
-    const char* topic = dataTopics[fieldID].topicString.c_str();
-    mqttClient.publish(topic, buf);
+  sprintf( buf, "%d", data );
+  const char* topic = dataTopics[fieldID].topicString.c_str();
+  mqttClient.publish( topic, buf );
+}
+
+void kwHeltecWifikit32::publish( uint8_t fieldID, float data )
+{
+  sprintf( buf, "%.1f", data );
+  const char* topic = dataTopics[fieldID].topicString.c_str();
+  mqttClient.publish( topic, buf );
 }
 
 // void clearValue(uint8_t row) {oled.clear(col0, col1, row, row + row - 1); }
-void clearValue(uint8_t row) {oled.clear( col0, col1, row, row) ; }
+void clearValue( uint8_t row ) { oled.clear( col0, col1, row, row ); }
 
-void kwHeltecWifikit32::update(uint8_t fieldID, uint16_t data)
+void kwHeltecWifikit32::update( uint8_t fieldID, uint16_t data )
 {
-    clearValue( fieldID * oled.fontRows() );
-    oled.print( data, 1 );
+  clearValue( fieldID * oled.fontRows() );
+  oled.print( data, 1 );
 }
 
-void kwHeltecWifikit32::update(uint8_t fieldID, float data)
+void kwHeltecWifikit32::update( uint8_t fieldID, float data )
 {
-    clearValue( fieldID * oled.fontRows() );
-    oled.print( data, 1 );
+  clearValue( fieldID * oled.fontRows() );
+  oled.print( data, 1 );
 }
 
-void kwHeltecWifikit32::update(uint8_t fieldID, const char* message)
+void kwHeltecWifikit32::update( uint8_t fieldID, const char* message )
 {
-    clearValue( fieldID * oled.fontRows() );
-    oled.print( message );
+  clearValue( fieldID * oled.fontRows() );
+  oled.print( message );
 }
 
 // Display system status
-void kwHeltecWifikit32::updateSystemStatus(std::string statusMessage)
+void kwHeltecWifikit32::updateSystemStatus( std::string statusMessage )
 {
-    oled.setCursor(0, 3 * oled.fontRows()),
-    oled.clearToEOL();
-    oled.print(statusMessage.c_str());
+  oled.setCursor( 0, 3 * oled.fontRows() ), oled.clearToEOL();
+  oled.print( statusMessage.c_str() );
 }
 
 // Run - keep MQTT alive and process commands
 void kwHeltecWifikit32::run()
-{   
-    if (didInitialiseMTTQ)
+{
+  if ( didInitialiseMTTQ )
+  {
+    if ( !mqttClient.connected() )
     {
-        if (!mqttClient.connected()) 
-        {   
-            digitalWrite(LED, LOW);
-            
-            long now = millis();
-            if (now - lastReconnectAttempt > MQTT_RECONNECT_TIME_SECONDS * 1000) 
-            {            
-                lastReconnectAttempt = now; 
-                updateSystemStatus("[->] MQTT");
-                
-                if (mqttReconnect()) 
-                {
-                    lastReconnectAttempt = millis();
-                }
-            }
-            else {} // Wait for timer to expire 
-        } 
-        else 
-        {   
-            mqttClient.loop();
-        }
-    }
+      digitalWrite( LED, LOW );
 
-    // Update clock
-    if (updateTimeTimer.repeat())
+      long now = millis();
+      if ( now - lastReconnectAttempt > MQTT_RECONNECT_TIME_SECONDS * 1000 )
+      {
+        lastReconnectAttempt = now;
+        updateSystemStatus( "[->] MQTT" );
+
+        if ( mqttReconnect() ) { lastReconnectAttempt = millis(); }
+      } else
+      {
+      }  // Wait for timer to expire
+    } else
     {
-        DateTime now = ds1307.now();
-        char buf[12];
-        sprintf(buf, "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
-        updateSystemStatus(buf);
+      mqttClient.loop();
     }
+  }
+
+  // Update clock
+  if ( updateTimeTimer.repeat() )
+  {
+    DateTime now = ds1307.now();
+    char     buf[12];
+    sprintf( buf, "%02d:%02d:%02d", now.hour(), now.minute(), now.second() );
+    updateSystemStatus( buf );
+  }
 }
 
-
-// PRIVATE ///////////////////////////////////////////////////////////////////////
+// PRIVATE
+// ///////////////////////////////////////////////////////////////////////
 
 // Populate the device ID
 void kwHeltecWifikit32::getMacAddress()
 {
-    String theAddress = WiFi.macAddress();
-    theAddress.replace(":", "");
-    strcpy(deviceID, theAddress.c_str());
+  String theAddress = WiFi.macAddress();
+  theAddress.replace( ":", "" );
+  strcpy( deviceID, theAddress.c_str() );
 }
 
-
 // Rconnect MQTT
-boolean kwHeltecWifikit32::mqttReconnect() 
+boolean kwHeltecWifikit32::mqttReconnect()
 {
-    updateSystemStatus("[->] MQTT");
+  updateSystemStatus( "[->] MQTT" );
 
-    const char* statusTopic = metaTopics[statusTopicID].c_str();
+  const char* statusTopic = metaTopics[statusTopicID].c_str();
 
-    if (mqttClient.connect(deviceID, statusTopic, 2, true, "OFFLINE")) 
-    {
-        updateSystemStatus("ONLINE");
-        digitalWrite(LED, HIGH);
-        
-        mqttClient.publish(statusTopic, "ONLINE");
-        for (int i=0; i < commandTopics.size(); i++)
-        { 
-            mqttClient.subscribe(commandTopics[i].commandString.c_str()); 
-        }
-        lastReconnectAttempt = millis();
-    }
+  if ( mqttClient.connect( deviceID, statusTopic, 2, true, "OFFLINE" ) )
+  {
+    updateSystemStatus( "ONLINE" );
+    digitalWrite( LED, HIGH );
 
-    return mqttClient.connected();
+    mqttClient.publish( statusTopic, "ONLINE" );
+    lastReconnectAttempt = millis();
+  }
+
+  return mqttClient.connected();
 }
 
 // MQTT callback function
-void kwHeltecWifikit32::mqttCallback(char* topic, byte* payload, unsigned int length)
+void kwHeltecWifikit32::mqttCallback( char* topic, byte* payload,
+                                      unsigned int length )
 {
-    std::string command(reinterpret_cast< const char* >(payload), length);
-    // std::string topic(reinterpret_cast< std::string >(topic));
-
-    // for (int i=0; i < commandTopics.size(); i++)
-    // {
-    //     commandTopics[i].didReceive = (commandTopics[i].commandString == topic)
-    // }
-
-    // Serial.printf("Command received... length %d command %s \n", length, command);
-    Serial.println(topic);
-    Serial.println(command.c_str());
-
-
-
 }
-
-
